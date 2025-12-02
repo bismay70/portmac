bc0c6dee2ca9987eae6124ecdc88cbcd2ea07457
bismay70 <samalbismay5@gmail.com>
2025-12-02 22:27:29 +0530

    gsap

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import {} from 'react-tooltip';
+import gsap from 'gsap';
+import {useGSAP} from "@gsap/react";
+const Dock = () => {
+    const dockRef = useRef(null);
+    useGSAP(() => {
+        const dock = dockRef.current;
+        if(!dock) return ;
+        const icons = dock.querySelectorAll('.dock-icon');
+        const animateIcons = (mouseX)=>{
+            const {left} = dock.getBoundingClientRect();
+            icons.forEach((icon)=>{
+                const {left:iconLeft, width} = icon.getBoundingClientRect();
+                const center = iconLeft - left + width / 2;
+                const distance = Math.abs(mouseX - center);
+                const intensity = math.exp(-(disatnce ** 2.5)/20000)
+                gsap.to(icon,{
+                    scale:1 + 0.25 * intensity,
+                    y:-15 * intensity,
+                    duration:0.2,
+                    ease:"power1.out"
+                })
 
-const Welcome = () => {
-  return (
-    <div>Welcome</div>
-  )
+        })
 }
 
-export default Welcome
+const handleMouseMove = (e)=>{
+
+}
+
+return(
+    <section id="dock"></section>
+)
+export default Dock
---
+++ src/store/window.js
@@ -0,0 +1,5 @@
+import {create} from "zustand"
+import {immer} from "zustand/middleware/immer"
+const useWindowStore = create(immer((set) => ({
+    
+})))
